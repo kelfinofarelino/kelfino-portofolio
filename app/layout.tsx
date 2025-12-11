5b38dde2d46ff1abecf625247fefbc6b41c732f1
kelfinofarelino <farelinokelvin@gmail.com>
2025-12-12 00:37:11 +0700

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter, Syne } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 const syne = Syne({ subsets: ["latin"], weight: ["700"], variable: '--font-syne' });
@@ -49,7 +50,9 @@ export default function RootLayout({
       <head>
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
       </head>
-      <body className={`${inter.className} ${syne.variable}`}>{children}</body>
+      <body className={`${inter.className} ${syne.variable}`}>{children}
+        <Analytics />
+      </body>
     </html>
   );
 }
